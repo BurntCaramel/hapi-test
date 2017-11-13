58ac9ee20564a383af95a9b4f381a54ddf9e0cf9
Patrick Smith <patrick@burntcaramel.com>
2017-11-13 16:20:11 +1100

    Improve with removal of naming defs

--- src/routes/swatches.js
+++ src/routes/swatches.js
@@ -1,3 +1,5 @@
+const Boom = require('boom')
+
 const renderDefs = (defs) => Object.keys(defs)
 	.reduce((lines, id) => {
 		const maker = defs[id]
@@ -7,12 +9,18 @@ const renderDefs = (defs) => Object.keys(defs)
 	}, [])
 	.join('\n')
 
-const makeSVG = (width, height, defs, inner) => (
+const makeSVG = (width, height, defs, children) => (
 `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="${width}" height="${height}">
-<defs>${renderDefs(defs)}</defs>
-${inner.join('\n')}
+<defs>${defs.join('\n')}</defs>
+${children.join('\n')}
 </svg>`
 )
+
+const buildSVG = (width, height, buildChildren) => {
+	let defs = []
+	const children = buildChildren(defs)
+	return makeSVG(width, height, defs, children)
+}
 
 const renderAttrs = (attrs) => Object.keys(attrs)
 	.filter(key => Boolean(attrs[key])) // Filter out null values
@@ -33,9 +41,13 @@ const makeStop = ({ offset, color }) => (
 	`<stop offset="${offset}" stop-color="${color}" />`
 )
 
-const prepareLinearGradient = (stops, { x1, x2, y1, y2 } = {}) => (id) => (
-	`<linearGradient ${renderAttrs({ id, x1, x2, y1, y2 })}>${stops.map(makeStop).join('')}</linearGradient>`
-)
+const prepareLinearGradient = (defs, stops, { x1, x2, y1, y2 } = {}) => {
+	const id = `item-${defs.length}`
+	defs.push(
+		`<linearGradient ${renderAttrs({ id, x1, x2, y1, y2 })}>${stops.map(makeStop).join('')}</linearGradient>`
+	)
+	return `url(#${id})`
+}
 
 const hexDigits = '0123456789abcdef'
 const randomNumberUpTo = (max) => Math.floor(Math.random() * (max + 1))
@@ -54,6 +66,23 @@ const processColor = (input) => {
 	}
 }
 
+const processFill = (defs, input) => {
+	const [type, valuesIn] = input.split(':')
+	if (type === 'linear-gradient') {
+		const [color1, color2] = valuesIn.split(',')
+		return prepareLinearGradient(defs, [
+			{ offset: '0%', color: processColor(color1) },
+			{ offset: '100%', color: processColor(color2) }
+		], toBottom)
+	}
+	else if (type === 'color') {
+		return processColor(valuesIn)
+	}
+	else {
+		throw Boom.badData(`Invalid fill type: '${type}'`)
+	}
+}
+
 const svgMimeType = 'image/svg+xml'
 
 const routes = [
@@ -69,34 +98,24 @@ const routes = [
 	},
 	{
 		method: 'get',
-		path: '/swatches:{width},{height}/rect/fill:random-gradient',
+		path: '/swatches:{width},{height}/rect/fill:{fill}',
 		handler({
-			params: { width, height }
+			params: { width, height, fill }
 		}, reply) {
-			reply(makeSVG(width, height, {
-				gradient1: prepareLinearGradient([
-					{ offset: '0%', color: randomRGB() },
-					{ offset: '100%', color: randomRGB() }
-				], toBottom)
-			}, [
-				makeRect('100%', '100%', { fill: 'url(#gradient1)' })
+			reply(buildSVG(width, height, (defs) => [
+				makeRect('100%', '100%', { fill: processFill(defs, fill) })
 			]))
 			.type(svgMimeType)
 		}
 	},
 	{
 		method: 'get',
-		path: '/swatches:{width},{height}/circle/fill:linear-gradient:{color1},{color2}',
+		path: '/swatches:{width},{height}/circle/fill:{fill}',
 		handler({
-			params: { width, height, color1, color2 }
+			params: { width, height, fill }
 		}, reply) {
-			reply(makeSVG(width, height, {
-				gradient1: prepareLinearGradient([
-					{ offset: '0%', color: processColor(color1) },
-					{ offset: '100%', color: processColor(color2) }
-				], toBottom)
-			}, [
-				makeCircle('50%', '50%', '50%', { fill: 'url(#gradient1)' })
+			reply(buildSVG(width, height, (defs) => [
+				makeCircle('50%', '50%', '50%', { fill: processFill(defs, fill) })
 			]))
 			.type(svgMimeType)
 		}
