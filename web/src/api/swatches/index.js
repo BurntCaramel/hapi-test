57f1f4f26edb864b18487ffae173aab2dcdb0015
Patrick Smith <patrick@burntcaramel.com>
2017-11-29 14:07:07 +1100

    Add fill fields

--- web/src/api/swatches/index.js
+++ web/src/api/swatches/index.js
@@ -2,6 +2,15 @@ import { getURL } from '../1'
 
 export const linearGradientRandom = 'linear-gradient:random,random'
 
+export const getPathForFill = ({ type, args }) => {
+  switch (type) {
+    case 'linear-gradient':
+      return `linear-gradient:${args.map(encodeURIComponent).join(',')}`
+    default:
+      throw new Error(`Unknown fill type: ${type}`)
+  }
+}
+
 export const getURLForSVG = (width, height, shape, fillPath) => (
   getURL(`/swatches:${width},${height}/${shape}/fill:${fillPath}`)
 )
--- web/src/components/swatches/index.js
+++ web/src/components/swatches/index.js
@@ -3,17 +3,22 @@ import { Link } from 'preact-router/match';
 import linkState from 'linkstate'
 import style from './style';
 import { Field, Choice, Button } from '../forms'
-import { getURLForSVG, linearGradientRandom } from '../../api/swatches'
+import { getURLForSVG, getPathForFill, linearGradientRandom } from '../../api/swatches'
 
 export default class Swatches extends Component {
 	state = {
 		width: 100,
 		height: 100,
 		shape: 'circle',
+		fill: {
+			type: 'linear-gradient',
+			args: ['#fff', 'random']
+		}
 	}
 
-	render({}, { width, height, shape }) {
-		const svgURL = getURLForSVG(width, height, shape, linearGradientRandom)
+	render({}, { width, height, shape, fill }) {
+		const fillPath = getPathForFill(fill)
+		const svgURL = getURLForSVG(width, height, shape, fillPath)
 
 		return (
 			<section class={style.section}>
@@ -30,6 +35,11 @@ export default class Swatches extends Component {
 						<option value='circle'>Circle</option>
 						<option value='rect'>Rectangle</option>
 					</Choice>
+					<Choice label='Fill Type' value={ fill.type } onChange={ linkState(this, 'fill.type') }>
+						<option value='linear-gradient'>Linear Gradient</option>
+					</Choice>
+					<Field label='Fill Color 1' value={ fill.args[0] } onChange={ linkState(this, 'fill.args.0') } />
+					<Field label='Fill Color 2' value={ fill.args[1] } onChange={ linkState(this, 'fill.args.1') } />
 					<div>
 						<Button>New</Button>
 					</div>
