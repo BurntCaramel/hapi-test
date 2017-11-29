c6d729f2a0d9b033485ec343d977102e5f8eecf5
Patrick Smith <patrick@burntcaramel.com>
2017-11-29 13:50:37 +1100

    Add shape choice

--- web/src/api/swatches/index.js
+++ web/src/api/swatches/index.js
@@ -2,6 +2,6 @@ import { getURL } from '../1'
 
 export const linearGradientRandom = 'linear-gradient:random,random'
 
-export const getURLForCircleSVG = (width, height, fillPath) => (
-  getURL(`/swatches:${width},${height}/circle/fill:${fillPath}`)
+export const getURLForSVG = (width, height, shape, fillPath) => (
+  getURL(`/swatches:${width},${height}/${shape}/fill:${fillPath}`)
 )
--- web/src/components/swatches/index.js
+++ web/src/components/swatches/index.js
@@ -2,16 +2,17 @@ import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import linkState from 'linkstate'
 import style from './style';
-import { getURLForCircleSVG, linearGradientRandom } from '../../api/swatches'
+import { getURLForSVG, linearGradientRandom } from '../../api/swatches'
 
 export default class Swatches extends Component {
 	state = {
 		width: 100,
-		height: 100
+		height: 100,
+		shape: 'circle',
 	}
 
-	render({}, { width, height }) {
-		const svgURL = getURLForCircleSVG(width, height, linearGradientRandom)
+	render({}, { width, height, shape }) {
+		const svgURL = getURLForSVG(width, height, shape, linearGradientRandom)
 
 		return (
 			<section class={style.section}>
@@ -24,6 +25,10 @@ export default class Swatches extends Component {
 				<div>
 					<input type='number' value={ width } onInput={ linkState(this, 'width') } />
 					<input type='number' value={ height } onInput={ linkState(this, 'height') } />
+					<select value={ shape } onChange={ linkState(this, 'shape') }>
+						<option value='circle'>Circle</option>
+						<option value='rect'>Rectangle</option>
+					</select>
 					<button>New</button>
 				</div>
 			</section>
